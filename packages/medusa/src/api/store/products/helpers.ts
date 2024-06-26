387f1f978ea2d22640b36b3987288cfeb7063795
Stevche Radevski <sradevski@live.com>
2024-06-26 11:37:38 +0200

    fix: Changes based on PR review

--- packages/medusa/src/api/store/products/helpers.ts
+++ packages/medusa/src/api/store/products/helpers.ts
@@ -1,3 +1,7 @@
+import {
+  ModuleRegistrationName,
+  calculateAmountsWithTax,
+} from "@medusajs/utils"
 import { MedusaRequest } from "../../../types/routing"
 import { refetchEntities, refetchEntity } from "../../utils/refetch-entity"
 import {
@@ -7,7 +11,6 @@ import {
   ItemTaxLineDTO,
   TaxCalculationContext,
 } from "@medusajs/types"
-import { ModuleRegistrationName, calculateTaxTotal } from "@medusajs/utils"
 
 export type RequestWithContext<T> = MedusaRequest<T> & {
   taxContext: {
@@ -73,53 +76,35 @@ export const wrapProductsWithTaxPrices = async <T>(
     req.taxContext.taxLineContext
   )) as unknown as ItemTaxLineDTO[]
 
-  const taxRatesAsMap = new Map<string, ItemTaxLineDTO[]>()
+  const taxRatesMap = new Map<string, ItemTaxLineDTO[]>()
   taxRates.forEach((taxRate) => {
-    if (!taxRatesAsMap.has(taxRate.line_item_id)) {
-      taxRatesAsMap.set(taxRate.line_item_id, [])
+    if (!taxRatesMap.has(taxRate.line_item_id)) {
+      taxRatesMap.set(taxRate.line_item_id, [])
     }
 
-    taxRatesAsMap.get(taxRate.line_item_id)?.push(taxRate)
+    taxRatesMap.get(taxRate.line_item_id)?.push(taxRate)
   })
 
   products.forEach((product) => {
     product.variants?.forEach((variant) => {
-      const taxRatesForVariant = taxRatesAsMap.get(variant.id) || []
-      const { priceWithTax, priceWithoutTax } = getTaxPricesForVariant(
-        variant,
-        taxRatesForVariant,
-        isTaxInclusive
-      )
+      if (!variant.calculated_price) {
+        return
+      }
 
+      const taxRatesForVariant = taxRatesMap.get(variant.id) || []
+      const { priceWithTax, priceWithoutTax } = calculateAmountsWithTax({
+        taxLines: taxRatesForVariant,
+        amount: variant.calculated_price!.calculated_amount!,
+        includesTax: isTaxInclusive,
+      })
+
+      // TODO: Resolve the typings when tax inclusivity is finalized.
       ;(variant.calculated_price as any).calculated_amount_with_tax =
         priceWithTax
       ;(variant.calculated_price as any).calculated_amount_without_tax =
         priceWithoutTax
     })
   })
-}
-
-const getTaxPricesForVariant = (
-  variant: HttpTypes.StoreProductVariant,
-  taxRatesForVariant: ItemTaxLineDTO[],
-  isTaxInclusive: boolean
-) => {
-  const tax = calculateTaxTotal({
-    // TODO: See why rate is nullable in tax
-    taxLines: taxRatesForVariant as any,
-    taxableAmount: variant.calculated_price!.calculated_amount!,
-    includesTax: isTaxInclusive,
-  })
-
-  const priceWithoutTax = isTaxInclusive
-    ? tax.minus(variant.calculated_price!.calculated_amount!).abs().toNumber()
-    : variant.calculated_price!.calculated_amount
-
-  const priceWithTax = isTaxInclusive
-    ? variant.calculated_price!.calculated_amount
-    : tax.plus(variant.calculated_price!.calculated_amount!).abs().toNumber()
-
-  return { priceWithTax, priceWithoutTax }
 }
 
 const asTaxItem = (product: HttpTypes.StoreProduct): TaxableItemDTO[] => {
@@ -134,6 +119,7 @@ const asTaxItem = (product: HttpTypes.StoreProduct): TaxableItemDTO[] => {
         product_id: product.id,
         product_name: product.title,
         product_categories: product.categories?.map((c) => c.name),
+        // TODO: It is strange that we only accept a single category, revisit the tax module implementation
         product_category_id: product.categories?.[0]?.id,
         product_sku: variant.sku,
         product_type: product.type,
